88ed8a656abea8d327102cfdc1a4ee1491c04c24
Ogunderu Noah <ifeoluwadayonoah.com>
2024-09-25 16:30:58 +0100

    post title truncate fix

--- src/components/posts/posttitle.jsx
+++ src/components/posts/posttitle.jsx
@@ -3,9 +3,7 @@ import { forwardRef } from "react"
 export const PostTitle = forwardRef(({ isReadingMore, title, side }, ref) => {
     return (
         <p ref={ref}
-            className={`
-                ${ side ? 'text-sm' : 'text-base' } text-tblack-100 font-medium leading-normal transition-all ease-linear duration-300
-                ${ !isReadingMore && 'line-clamp-3' } whitespace-pre-wrap break-words `}
+            className={`${side ? 'text-sm' : 'text-base'} text-tblack-100 font-medium leading-normal transition-all ease-linear duration-300 whitespace-pre-wrap break-words `}
         >
             {title}
         </p>
