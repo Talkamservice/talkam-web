e614ef3cf9f270b844e08359932b29ef8eebd294
Joelomojefe <joelomojefe@gmail.com>
2026-08-06 10:23:30 +0100

    refactor: implement lazy loading for Messages component and restructure postStorageKeys import

--- src/routes/dashboard/create post/createpost.jsx
+++ src/routes/dashboard/create post/createpost.jsx
@@ -32,15 +32,7 @@ import { selectCurrentUser } from "../../../services/authSlice";
 import { PromotionModal } from "../userprofile/promotion/promotion";
 import Protected from "../../../utils/protected";
 import moment from "moment";
-
-export const postStorageKeys = [
-    "post_title", "post_comment",
-    "post_image", "post_tags",
-    "post_publish", "post_anonymous",
-    "post_polls", "post_category",
-    "post_group", "post_image_url",
-    "post_video_url"
-];
+import { postStorageKeys } from "./postStorageKeys";
 
 export const CreatePost = () => {
 
